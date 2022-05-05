f6f50da87f86d9e2ca9a39256d469c196e770ee2
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-05-05 13:28:37 +0530

    initial commit

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -20,6 +20,16 @@ const TopNav = () => {
         Home
       </Link>
 
+      {(auth === null || auth === {}) && (
+        <>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </>
+      )}
       {auth != null && (
         <Link className="nav-link" to="/dashboard">
           Dashboard
@@ -30,16 +40,6 @@ const TopNav = () => {
         <a className="nav-link pointer" onClick={logout}>
           Logout
         </a>
-      )}
-      {(auth === null || auth==={}) && (
-        <>
-          <Link className="nav-link" to="/login">
-            Login
-          </Link>
-          <Link className="nav-link" to="/register">
-            Register
-          </Link>
-        </>
       )}
     </div>
   );
